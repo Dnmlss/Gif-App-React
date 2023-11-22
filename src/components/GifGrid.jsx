9d76382e3e1ce6449567ca460b125a9a629d9c40
Diego Miño <diegominho92@gmail.com>
2023-11-22 16:04:06 -0300

    useEffect

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,14 +1,21 @@
+import { useState, useEffect } from 'react';
 import { getGifs } from '../helpers/getGifs';
 
 export const GifGrid = ({ category }) => {
-	getGifs(category);
+	const [counter, setCounter] = useState(10);
 
-	// constante con array y cantidad de gifs
+	// Dos argumentos, el primero es un callback(cualquier función) y luego una lista de dependencias(condiciones por las cuales queremos volver a ejecutar el callback)
+	useEffect(() => {
+		getGifs(category);
+	}, []); // si dejamos las dependencias [array] vacias, significa que este hook solo se va a disparar la primera vez que se crea y construye este componente
 
 	return (
 		<>
 			<h3>{category}</h3>
-			<p>op</p>
+			<h5>{counter}</h5>
+			<button onClick={() => setCounter(counter + 1)}>+1</button>
 		</>
 	);
 };
+
+// Hook de React que sirve para disparar efectos secundarios. Sería algún proceso que querramos ejecutar cuando algo suceda,por ej: cuando el counter cambie, lanzamos un efecto, cuando agregamos una categoria, lanzamos un efecto, cuando el componente se renderiza por primera vez, lanzamos un efecto, lanzamos los efectos secuandarios en cualquier punto que querramos.
